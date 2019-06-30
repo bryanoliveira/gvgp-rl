633d7f1f23325728788d4581122dbf4eb7d9830f
Luana Martins <luanagbmartins@gmail.com>
2019-06-30 01:19:18 -0300

    Atari wrappers for frame stack

--- envs/atari_conv.py
+++ envs/atari_conv.py
@@ -2,22 +2,26 @@ import gym
 import cv2
 import numpy as np
 from envs._interface import EnvInterface
+from envs.atari_wrappers import * 
 
 
 class Env(EnvInterface):
-    def __init__(self, env_name, stack_frames=1):  
+    def __init__(self, env_name, stack_frames=4):  
         super(Env, self).__init__(env_name)
 
         self.stack_frames = stack_frames
+        self.env = make_atari(env_name)
+        self.env = wrap_deepmind(self.env, frame_stack=True,  pytorch_img=True)
+
 
     def reset(self):
-        state = super(Env, self).reset()
-        return state[None, :]
+        state = self.env.reset()        
+        return state.__array__()
 
     def step(self, action):
-        state, reward, done, info = super(Env, self).step(action)
+        state, reward, done, info = self.env.step(action)
         
-        return state[None, :], reward, done, info
+        return state, reward, done, info
 
     def factory(env_name):
         return lambda : Env(env_name)
